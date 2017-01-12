c5470c695514828ea7a4598cca569747ff7a9fc5
francisypl <francisypl@gmail.com>
2017-01-11 16:52:11 -0800

    Added a GroupsViewController

--- inkcluded-405/GroupsViewController.swift
+++ inkcluded-405/GroupsViewController.swift
@@ -1,25 +1,25 @@
 //
-//  ViewController.swift
+//  GroupsViewController.swift
 //  inkcluded-405
 //
 //  Created by Francis Yuen on 1/11/17.
 //  Copyright © 2017 Boba. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class GroupsViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
