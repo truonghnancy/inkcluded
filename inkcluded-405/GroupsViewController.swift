76fef36b9c2b196623901ad6e061aab90458bfc3
francisypl <francisypl@gmail.com>
2017-01-22 13:10:20 -0800

    removed prints

--- inkcluded-405/GroupsViewController.swift
+++ inkcluded-405/GroupsViewController.swift
@@ -23,20 +23,11 @@ class GroupsViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         // Do any additional setup after loading the view, typically from a nib.
-        print("\t\t hello")
-        print(UserDefaults.standard.object(forKey: "oauthKey") ?? 1)
-        if (FBSDKAccessToken.current() == nil)
-//        if (UserDefaults.standard.object(forKey: "oauthKey") == nil)
-        {
-            print("\t\t\tnot logged in")
-            self.performSegue(withIdentifier: "showLogin" , sender: self)
-        }
-        else
-        {
-            //            print(FBSDKAccessToken.currentAccessToken())
-            print("logged in")
+        
+//        if (FBSDKAccessToken.current() == nil)
+//        {
 //            self.performSegue(withIdentifier: "showLogin" , sender: self)
-        }
+//        }
         
     }
     
