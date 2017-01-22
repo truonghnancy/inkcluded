b0c15e1ff7b06fcd348d7945025fbd652efdce8c
francisypl <francisypl@gmail.com>
2017-01-22 14:20:20 -0800

    basic mock api created

---
+++ inkcluded-405/APIProtocol.swift
@@ -0,0 +1,23 @@
+//
+//  APIProtocol.swift
+//  inkcluded-405
+//
+//  Created by Francis Yuen on 1/22/17.
+//  Copyright © 2017 Boba. All rights reserved.
+//
+
+import Foundation
+
+protocol APIProtocol {
+    func getFriendsList() -> [User];
+    func getFriendById(userId: Int) -> User;
+    
+    func getCurrentUser() -> User;
+    
+    func createGroup(members: [Int], name: String) -> Group;
+    func getAllGroups() -> [Group];
+    func getGroupById(groupId: Int) -> Group;
+    
+    func getAllMessagesInGroup(groupId: Int) -> [Message];
+    func sendMessage(message: Message);
+}
