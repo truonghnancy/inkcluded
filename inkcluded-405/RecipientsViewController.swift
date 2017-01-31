ac741f24896d5ae2d06d593e6fce2f49b39300ff
Christopher Siu <christopher@get2thecrossing.com>
2017-01-31 13:40:58 -0800

    Temporary commit in order to switch to master.

--- inkcluded-405/RecipientsViewController.swift
+++ inkcluded-405/RecipientsViewController.swift
@@ -10,22 +10,43 @@ import Foundation
 import UIKit
 
 class RecipientsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    @available(iOS 2.0, *)
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //
-        return 0;
-    }
-
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //
-        return UITableViewCell();
-    }
     
     @IBOutlet weak var friendsTableView: UITableView!
+    
+    let apiWrapper: APIWrapper = APIWrapper()
+    var friends : [User]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.friends = apiWrapper.getFriendsList()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends!.count;
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.friendsTableView.dequeueReusableCell(withIdentifier: "friendCell") as! GroupsTableViewCell
+        let friend = self.friends?[indexPath.row];
+        let names: [String] = (group?.members.map({ (memberId) -> String in
+            return apiWrapper.getFriendById(userId: memberId).firstName;
+        }))!
+        let finalNames = names.filter { (name) -> Bool in
+            return name != self.apiWrapper.getCurrentUser().firstName;
+        }
+        
+        cell.groupName.text = friend?.firstName + " " + friend?.lastName
+        cell.groupDetails.text = finalNames.joined(separator: ", ")
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
     }
     
