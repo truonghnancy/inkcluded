a8596b4c57b84a1fb5f1c784e6066e72be91bc22
Christopher Siu <christopher@get2thecrossing.com>
2017-02-12 16:11:00 -0800

    Implemented selecting recipients for new messages.

---
+++ inkcluded-405/FriendTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  FriendTableViewCell.swift
+//  inkcluded-405
+//
+//  Created by Christopher on 1/31/17.
+//  Copyright © 2017 Boba. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    //@IBOutlet var friendName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- inkcluded-405/RecipientsViewController.swift
+++ inkcluded-405/RecipientsViewController.swift
@@ -1,66 +1,98 @@
-//
-//  RecipientsViewController.swift
-//  inkcluded-405
-//
-//  Created by Christopher on 1/30/17.
-//  Copyright © 2017 Boba. All rights reserved.
-//
+/**
+ * Controls the view for selecting recipients for a new message.
+ *
+ * RecipientsViewController.swift
+ * inkcluded-405
+ *
+ * Created by Christopher on 1/30/17.
+ * Copyright © 2017 Boba. All rights reserved.
+ */
 
 import Foundation
 import UIKit
 
-class RecipientsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class RecipientsViewController: UIViewController, UITableViewDelegate,
+                                UITableViewDataSource {
+    @IBOutlet var friendsTableView: UITableView!
     
-    @IBOutlet weak var friendsTableView: UITableView!
+    let apiWrapper: APIWrapper = APIWrapper() // The database interface
+    var selectedRecipients = [User]()         // A list of selected recipients
+    var friends : [User]?                     // A list of friends to select
     
-    let apiWrapper: APIWrapper = APIWrapper()
-    var friends : [User]?
-    
+    /**
+     * Performs setup once the view loads.
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Get the list of friends from the database.
         self.friends = apiWrapper.getFriendsList()
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    /**
+     * Returns the number of cells required in the table.
+     */
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
         return friends!.count;
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 70
+    func tableView(_ tableView: UITableView,
+                   heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = self.friendsTableView.dequeueReusableCell(withIdentifier: "friendCell") as! GroupsTableViewCell
+    /**
+     * Sets the content of one cell in the table.
+     */
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get the cell from the table.
+        let cell = self.friendsTableView.dequeueReusableCell(
+                    withIdentifier: "friendCell") as! FriendTableViewCell
+        // Get the corresponding friend from the list.
         let friend = self.friends?[indexPath.row];
-        let names: [String] = (group?.members.map({ (memberId) -> String in
-            return apiWrapper.getFriendById(userId: memberId).firstName;
-        }))!
-        let finalNames = names.filter { (name) -> Bool in
-            return name != self.apiWrapper.getCurrentUser().firstName;
-        }
-        
-        cell.groupName.text = friend?.firstName + " " + friend?.lastName
-        cell.groupDetails.text = finalNames.joined(separator: ", ")
+        // Set the cell's text to be the friend's name.
+        cell.textLabel?.text = friend!.firstName + " " + friend!.lastName
         
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    /**
+     * Responds to a cell's being selected.
+     */
+    func tableView(_ tableView: UITableView,
+                   didSelectRowAt indexPath: IndexPath) {
+        // Add the friend corresponding to the cell to the list of recipients.
+        // TODO: There is currently no support for deselecting a friend.
+        self.selectedRecipients.append((self.friends?[indexPath.row])!);
+    }
+    
+    /**
+     * Responds to the 'Select' button's being pressed.
+     */
+    @IBAction func selectPressed(_ sender: Any) {
+        // If no recipients have been selected, do nothing.
+        if selectedRecipients.isEmpty {
+            print("No recipients selected.")
+        }
+        // Otherwise, create a new group using the selected recipients.
+        else {
+            // TODO: The mock API wrapper only has getters, not setters. Once
+            //       the backend is complete, actually do something with these
+            //       recipients.
+            print("Recipients:")
+            for recipient : User in selectedRecipients {
+                print("   \(recipient.firstName) \(recipient.lastName)")
+            }
         
+            // Segue to the canvas view.
+            self.performSegue(withIdentifier: "newCanvasSegue", sender: self)
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         // Do any additional setup after loading the view, typically from a nib.
-        
-        //        if (FBSDKAccessToken.current() == nil)
-        //        {
-        //            self.performSegue(withIdentifier: "showLogin" , sender: self)
-        //        }
-        print("help")
-        
     }
-    
 }
 
