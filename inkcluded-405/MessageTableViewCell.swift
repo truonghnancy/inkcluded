fb9b2e1df11192eeff4a12a371e55b7fad13ae36
Christopher Siu <christopher@get2thecrossing.com>
2017-02-20 19:00:21 -0800

    Added Group History view; allowed users to deselect recipients when creating new messages.

---
+++ inkcluded-405/MessageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageTableViewCell.swift
+//  inkcluded-405
+//
+//  Created by Christopher on 2/20/17.
+//  Copyright © 2017 Boba. All rights reserved.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
