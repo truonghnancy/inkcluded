eadb8e16985a287e920d81bf430df5339635e3a4
francisypl <francisypl@gmail.com>
2017-02-26 21:06:32 -0800

    Draggable images implemented

---
+++ inkcluded-405/DraggbleElement.swift
@@ -0,0 +1,14 @@
+//
+//  DraggbleElement.swift
+//  inkcluded-405
+//
+//  Created by Francis Yuen on 2/26/17.
+//  Copyright © 2017 Boba. All rights reserved.
+//
+
+import Foundation
+
+protocol DraggableElement {
+    func configureDraggbleGestureRecognizers()
+    
+}
