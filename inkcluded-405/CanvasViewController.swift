bb6437d77f9637b6ade63e280fb200be7b6196c5
Nancy Truong <truongh.nancy@gmail.com>
2017-02-28 10:29:46 -0800

    pulls up keyboard

--- inkcluded-405/CanvasViewController.swift
+++ inkcluded-405/CanvasViewController.swift
@@ -109,6 +109,7 @@ extension CanvasViewController: CanvasMenuDelegate {
             myField.borderStyle = UITextBorderStyle.bezel
             myField.delegate = self
             self.drawView!.addSubview(myField)
+            self.drawView!.becomeFirstResponder()
             // TODO: figure out how to serialize
             break
         default:
