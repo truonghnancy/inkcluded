1ca9ebca8f7a878003619a75403b60725653fa42
francisypl <francisypl@gmail.com>
2017-04-17 20:06:32 -0700

    MenuView on another view Added a feedback button Added version number label

---
+++ inkcluded-405/MenuViewDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  MenuViewDelegate.swift
+//  inkcluded-405
+//
+//  Created by Francis Yuen on 4/17/17.
+//  Copyright © 2017 Boba. All rights reserved.
+//
+
+import Foundation
+
+protocol MenuViewDelegate {
+    func didClickOnFeedbackButton()
+}
